0c492a14a7c7665f6b1d02eacb95d483cbd65d74
Tim Sabsch <tim@sabsch.com>
2016-11-02 14:43:22 +0100

    Implement upper_envelope view
    The new function upper_envelope() takes an alpha-cut as input and
    returns a figure displaying the upper envelope described by the
    alpha-cut.

--- alphacuts/visualize.py
+++ alphacuts/visualize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 __author__ = 'tsabsch <tim@sabsch.com>'
 
+import bisect
 import matplotlib.pyplot as plt
 
 def horizontal_view(acuts):
@@ -18,11 +19,55 @@ def horizontal_view(acuts):
 
     return fig
 
+def upper_envelope(acuts):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    xs = list()
+    ys = list()
+    tmp_steps = list()
+
+    for degree, cuts in acuts.iteritems():
+        print degree, cuts
+
+        # insert temporary steps
+        for x in tmp_steps:
+            i = bisect.bisect_left(xs, x)
+            xs.insert(i, x)
+            ys.insert(i, degree)
+        tmp_steps = list()
+
+        for (start, end) in cuts:
+            if end not in xs:
+                # insert new (down) step
+                i = bisect.bisect(xs, end)
+                xs.insert(i, end)
+                ys.insert(i, degree)
+
+            if start in xs:
+                if start == end:
+                    # special case: peak
+                    tmp_steps += [start]
+                else:
+                    # update step
+                    i = xs.index(start)
+                    ys[i] = degree
+            else:
+                # new up step detected, put on hold until degree is determined
+                tmp_steps += [start]
+
+        print xs, ys
+
+    ax.step(xs,ys)
+    ax.set_ylim([0,1.1])
+    ax.set_yticks(acuts.keys())
+    return fig
+
 def visualize(acuts, type='horizontal_view'):
     if type == 'horizontal_view':
         fig = horizontal_view(acuts)
     if type == 'upper_envelope':
-        raise NotImplementedError()
+        fig = upper_envelope(acuts)
 
     plt.show()
 
